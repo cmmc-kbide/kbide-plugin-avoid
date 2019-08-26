fb14cd88a1478aafd8b309003fda4c9e260f27ff
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 13:05:16 +0700

    add generator

--- blocks/blocks_avoid.js
+++ blocks/blocks_avoid.js
@@ -1,18 +1,27 @@
-Blockly.Blocks['ky_032_avoid_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("KY-032 AVOID");
-    this.appendValueInput("KY_032_AVOID_LED_PIN")
-        .setCheck("Number")
-        .appendField("LED PIN");
-    this.appendValueInput("KY_032_AVOID_BUTTON_PIN")
-        .setCheck("Number")
-        .appendField("BUTTON PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
+module.exports = function(Blockly) {
+  Blockly.Blocks["ky_032_avoid_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("KY-032 AVOID");
+      this.appendDummyInput()
+        .appendField("LED PIN")
+        .appendField(new Blockly.FieldTextInput("0"), "KY_032_AVOID_LED_PIN");
+      this.appendDummyInput()
+        .appendField("BUTTON PIN")
+        .appendField(
+          new Blockly.FieldTextInput("0"),
+          "KY_032_AVOID_BUTTON_PIN"
+        );
+      this.appendDummyInput()
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("KY_032_AVOID_VALUE"),
+          "KY_032_AVOID_VALUE"
+        );
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
 };
--- blocks/generators_avoid.js
+++ blocks/generators_avoid.js
@@ -1,7 +1,25 @@
-Blockly.JavaScript['ky_032_avoid_block'] = function(block) {
-  var value_ky_032_avoid_led_pin = Blockly.JavaScript.valueToCode(block, 'KY_032_AVOID_LED_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_ky_032_avoid_button_pin = Blockly.JavaScript.valueToCode(block, 'KY_032_AVOID_BUTTON_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
+Blockly.JavaScript["ky_032_avoid_block"] = function(block) {
+    var text_ky_032_avoid_led_pin = block.getFieldValue("KY_032_AVOID_LED_PIN");
+    var text_ky_032_avoid_button_pin = block.getFieldValue(
+      "KY_032_AVOID_BUTTON_PIN"
+    );
+    var variable_ky_032_avoid_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("KY_032_AVOID_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+	#VARIABLE
+	int KY_032_AVOID_LED_PIN = ${text_ky_032_avoid_led_pin} ;// define LED Interface
+	int KY_032_AVOID_BUTTON_PIN = ${text_ky_032_avoid_button_pin}; // define the obstacle avoidance sensor interface
+	#END
+	
+	#SETUP
+		pinMode (KY_032_AVOID_LED_PIN, OUTPUT) ;// define LED as output interface
+	  	pinMode (KY_032_AVOID_BUTTON_PIN, INPUT) ;// define the obstacle avoidance sensor output interface
+	#END	
+
+	${variable_ky_032_avoid_value} = digitalRead (KY_032_AVOID_BUTTON_PIN) ;// digital interface will be assigned a value of 3 to read val
+    `;
+    return code;
+  };
